94179a996363e1e93c0cf06d92ab6b683c91f665
putrisalwael@gmail.com <you@example.com>
2022-11-10 20:41:46 +0700

    commit final keyword

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -93,6 +93,17 @@ void main() {
   var n = hitungNilai2(mapel1: 50, mapel2: 2);
   print(n);
   var o = hitungNilai3(79, 100);
+
+  //final keyword => imutable / tidak bisa dirubah
+//const final
+//const
+//const String mahasiswa = "baban" ;
+//final
+final String mahasiswafk; 
+  
+  mahasiswafk="baban";
+  
+  print(mahasiswafk);
 }
 //function
   hitungNilai(){
